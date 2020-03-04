ebf4cad57b74e6ddf9f025eb1df6807e664eb7da
MatthewChen37 <matt.chen42601@gmail.com>
2020-03-03 22:50:52 -0500

    Fixed the plots

--- collatz.py
+++ collatz.py
@@ -4,8 +4,6 @@ import random
 import matplotlib.pyplot as plt
 
 
-n = random.randrange(0, 1000, 1)
-counter = 0;
 
 def collatz_function(n):
     
@@ -15,20 +13,39 @@ def collatz_function(n):
         n = (3 * n + 1)
     return n
 
-print(n)
-
-while(n != 1):
-    n = collatz_function(n)
+def collatz_several():
+    n = random.randrange(1, 1000, 1)
+    org = n
     print(n)
-    plt.plot(counter, n, 'bo');
-    counter += 1
-    
-print("Counter: " + str(counter))
+    counter = 0
+    plt.figure(1)
+    while(n != 1):
+        n = collatz_function(n)
+        print(n)
+        plt.plot(counter, n, 'bo');
+        counter += 1
+    print("Counter: " + str(counter))
+    plt.figure(2)
+    plt.plot(counter, org, 'bo');
 
 
+for i in range(100):
+    collatz_several()
 
+
+plt.figure(1)
+plt.title('Value of numbers for 100 random values between 0 and 1000')
 plt.ylabel('Integer value')
 plt.xlabel('Iteration Number')
+
+
+plt.figure(2)
+plt.title('Correspondence of original value to total iteration')
+plt.ylabel('Original Value')
+plt.xlabel('Total Iterations: ')
+
 plt.show()
+
+
 
 
